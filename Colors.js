0310f5c8b5fd1f8767b80c49ec30fe677334c280
MichaelWuhu <mwu250@gmail.com>
2022-08-23 14:14:02 -0700

    Michael/Colors
    added Jason's color palette and the color palette James' sent in discord

---
+++ Colors.js
@@ -0,0 +1,20 @@
+export const COLORS = {
+    // white and black
+    white: '#fff',
+    black: '#000',
+
+    // this is Jason's color palette
+    aquamarine: '74FCE1',
+    babyBlue: '74D1FC',
+    helliotrope: 'C874FC',
+    lightGreen: '6FED89',
+    deepSaffron: 'FF9A36',
+    
+    // this is Jam's recommended color palette
+    softYellow: '#E0D8B0',
+    paleYellow: '#FCFFE7',
+    softOrange: '#DEA057',
+    moderateOrange: '#CE9461',
+        
+
+  }
