e953173224599d4fdb4714b5ce84c6bbb6546adf
MichaelWuhu <98852023+MichaelWuhu@users.noreply.github.com>
2022-08-23 22:07:39 -0700

    Michael+Jason/homescreen
    added stuff

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,36 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Picker, StyleSheet, TouchableOpacity } from "react-native";
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text>This the home screen</Text>
-    </View>
+    
+      
+        <View style={styles.container}>
+            <Text>Hi</Text>
+            
+            <View style={styles.titleContainer}>
+              <Text style={styles.appTitle}>Pathfinder</Text>
+            
+              <TouchableOpacity style={styles.random} onPress={()=>
+                console.log("Hi")
+                }>
+                <Text>BUTTON</Text>
+              </TouchableOpacity>
+              
+              
+            </View>
+
+        </View>
+    
+    
   );
 };
 
+// <View style={style.container}></View>
+/*
+<View style={styles.random}>
+  <Text>Hi</Text>
+</View>
+*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -15,6 +38,35 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+
+  titleContainer:{
+    flex: 1,
+    backgroundColor: "#7cb48f",
+    alignItems: "flex-start",
+    justifyContent: "top",
+    flexDirection: 'center',
+    marginTop: 50,
+    marginBottom: 725,
+    marginRight: 150,
+  },
+
+  
+  random:{
+    backgroundColor: "#45edea",
+    // flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+  
+  
+  appTitle: {
+    fontSize: 35,
+    fontWeight: 'bold',
+
+  },
+  
 });
 
 export default Home;
