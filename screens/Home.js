165320a9432c48487ed45eb98f094cdf17e92069
MichaelWuhu <98852023+MichaelWuhu@users.noreply.github.com>
2022-08-24 17:48:45 -0700

    Michael/homescreen
    added the title labels and created the layout of the stuff

--- screens/Home.js
+++ screens/Home.js
@@ -1,23 +1,37 @@
-import { View, Text, Picker, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, Picker, StyleSheet, TouchableOpacity, SafeAreaView } from "react-native";
 
 const Home = () => {
   return (
     
       
         <View style={styles.container}>
-            <Text>Hi</Text>
+            
             
             <View style={styles.titleContainer}>
-              <Text style={styles.appTitle}>Pathfinder</Text>
-            
-              <TouchableOpacity style={styles.random} onPress={()=>
-                console.log("Hi")
-                }>
-                <Text>BUTTON</Text>
-              </TouchableOpacity>
-              
-              
+              <Text style={{fontSize: 35, fontWeight: 'bold'}}>Pathfinder</Text>
             </View>
+            
+            <View style={styles.dropdownsAndTitlesOnLeft}>
+              <Text style={styles.leftSideTitles}>Genre</Text>
+              <Text style={styles.leftSideTitles}>Artist</Text>
+              <Text style={styles.leftSideTitles}>Date</Text>
+            </View>
+
+            <View style={styles.slidersAndTitlesOnRight}>
+              <Text style={styles.rightSideTitles}>Energy</Text>
+              <Text style={styles.rightSideTitles}>Mood</Text>
+              <Text style={styles.rightSideTitles}>Danceablility</Text>
+            </View>
+
+            <SafeAreaView style={styles.recommendButtonContainer}>
+              <TouchableOpacity style={styles.recommendButton} onPress={()=>
+                  console.log("Hi")
+                  }>
+                  <Text style={{ fontSize: 20}}>RECOMMEND</Text>
+              </TouchableOpacity>
+            </SafeAreaView>
+
+
 
         </View>
     
@@ -25,47 +39,83 @@ const Home = () => {
   );
 };
 
-// <View style={style.container}></View>
-/*
-<View style={styles.random}>
-  <Text>Hi</Text>
-</View>
-*/
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 50,
+    paddingBottom: 180,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 
   titleContainer:{
     flex: 1,
-    backgroundColor: "#7cb48f",
+    // backgroundColor: "#7cb48f",
     alignItems: "flex-start",
-    justifyContent: "top",
-    flexDirection: 'center',
-    marginTop: 50,
-    marginBottom: 725,
-    marginRight: 150,
+    justifyContent: "center",
+    flexDirection: 'flex-start',
+    padding: 10,
+    
   },
 
-  
-  random:{
-    backgroundColor: "#45edea",
-    // flex: 1,
+
+  dropdownsAndTitlesOnLeft:{
+    flex: 3,
+    alignItems: 'flex-start',
+    justifyContent: 'space-evenly',
+
+  },
+
+  leftSideTitles:{
+    flexDirection: 'flex-start',
+    paddingLeft: 25,
+    fontSize: 30,
+    // fontWeight: 'bold'
+  },
+
+
+  slidersAndTitlesOnRight:{
+    flex: 3,
+    alignItems: 'flex-end',
+    justifyContent: 'space-evenly',
+
+  },
+
+  rightSideTitles:{
+    alignItems: 'flex-end',
+    paddingRight: 25,
+    fontSize: 30,
+    // fontWeight: 'bold'
+  },
+
+
+  recommendButtonContainer:{
+    // backgroundColor: "#ddd",
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    // borderColor: 'black',
+    // borderWidth: 2,
+    padding:20,
+    
+  },
+
+  recommendButton:{
+    backgroundColor: "#45edea",
     borderColor: 'black',
     borderWidth: 2,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    // flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
   },
   
   
-  appTitle: {
-    fontSize: 35,
-    fontWeight: 'bold',
-
-  },
+  
   
 });
 
