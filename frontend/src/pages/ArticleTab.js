86e5373c07edc6694cf3b9be2dcf93e2bce0e901
jameswu <jamesw0462@gmail.com>
2023-07-05 17:07:07 -0700

    Made main-content scrollable without disrupting sidebars

--- frontend/src/pages/ArticleTab.js
+++ frontend/src/pages/ArticleTab.js
@@ -1,7 +1,70 @@
 import React from "react";
+import '../styles/articleTab.css';
+import Collapsible from 'react-collapsible'; //Documentation: https://www.npmjs.com/package/react-collapsible
 
 export default function ArticleTab() {
+
     return (
-        <h1>THIS IS ARTICLE TAB</h1>
+        <div className="article">
+            <h1>Interest name here</h1>
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+            <p>Interest description here. deez nuts deez nuts ligma balls lololol </p>
+
+
+            <div className="subArticles">
+                <Collapsible trigger="Subarticle name">
+                    <p>
+                        This is the collapsible content. It can be any element or React
+                        component you like.
+                    </p>
+                    <p>
+                        It can even be another Collapsible component. Check out the next
+                        section!
+                    </p>
+                </Collapsible>
+                <Collapsible trigger="Subarticle name">
+                    <p>
+                        This is the collapsible content. It can be any element or React
+                        component you like.
+                    </p>
+                    <p>
+                        It can even be another Collapsible component. Check out the next
+                        section!
+                    </p>
+                </Collapsible>
+            </div>
+
+        </div>
     );
 }
