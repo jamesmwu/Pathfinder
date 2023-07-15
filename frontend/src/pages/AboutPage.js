a485835239f18304db2929240e059b60124d4fa4
jameswu <jamesw0462@gmail.com>
2023-07-15 15:37:43 -0700

    Updated favicon and some other stuff

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -9,7 +9,7 @@ export default function AboutPage() {
                 <p>Pathfinder is an online platform that helps you explore career options by connecting you with college mentors.</p>
                 <p>Oftentimes when we think of different jobs-- doctors, accountants, software engineers, journalists-- we might have a general idea of what they do. But each one of these careers have different requirements and actionable steps that are recommended to follow, and most the time finding the answers to those questions takes a lot of effort and networking!</p>
                 <p>Pathfinder aims to make understanding career paths simpler by directly connecting you with individuals experienced in their fields, so you can learn what the job entails or how you can get there yourself!</p>
-                <p>Sign up now to join 1000+ other mentees and gain some career insight, today! :)</p>
+                <p>Sign up now talk to a mentor and gain some career insight, today! :)</p>
             </div>
         </div>
     );
