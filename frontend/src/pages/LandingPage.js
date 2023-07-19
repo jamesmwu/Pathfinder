374c7c23cbe0115795f3901aee04554126e24ab7
jameswu <jamesw0462@gmail.com>
2023-07-18 22:32:37 -0700

    Updated landing page details

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -14,12 +14,16 @@ export default function LandingPage() {
         <div className='full landing'>
             <LandingNavbar />
             <Parallax ref={parallax} pages={3}>
-                <ParallaxLayer offset={0} speed={0} factor={3} className='full' style={{ backgroundSize: 'cover' }} />
-                <ParallaxLayer offset={1} speed={1} className='full' />
-                <ParallaxLayer offset={2} speed={1} className='full' />
+                <ParallaxLayer offset={0} speed={0} factor={3} className='full'
+                    style={{
+                        backgroundSize: 'cover',
+                        backgroundImage: url('stars', true),
+                    }} />
+                <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73', width: "100%", height: "100%" }} />
+                <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE', width: "100%", height: "100%" }} />
 
                 <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
-                    <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} alt="Satellite" />
+                    <img src={url('satellite4')} style={{ width: '15%', marginLeft: '10%', marginTop: '0' }} alt="Satellite" />
                 </ParallaxLayer>
 
                 <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
@@ -48,8 +52,8 @@ export default function LandingPage() {
                     speed={0.4}
                     style={{ opacity: 0.6, display: 'flex', alignItems: 'center', justifyContent: 'center' }}
                 >
-                    {/* <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} alt="Cloud" /> */}
-                    {/* <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} alt="Cloud" /> */}
+                    <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} alt="Cloud" />
+                    <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} alt="Cloud" />
                 </ParallaxLayer>
 
                 <ParallaxLayer
@@ -61,12 +65,37 @@ export default function LandingPage() {
                 </ParallaxLayer>
 
                 <ParallaxLayer
+                    offset={1}
+                    speed={0.4}
+                    className='whatWeDoDescContainer'
+                >
+                    <div className='whatWeDoDesc'>
+                        <h2>If you've ever been curious about another field, we're here to help! Pathfinder allows you to explore your career interests by chatting with verified, experienced mentors and offering curated articles. </h2>
+                    </div>
+                </ParallaxLayer>
+
+                <ParallaxLayer
+                    offset={2}
+                    speed={0.4}
+                    className='contactUsDescContainer'
+                >
+                    <div className='contactUsDescHeader'>
+                        <h2>
+                            We're always happy to hear from you! Feel free to reach out to us or let us know if you have any questions, feedback, or even just a friendly hello! We read every single message, we promise.
+                        </h2>
+                    </div>
+
+                    <div className='contactUsDesc'>
+                        <h2>pathfindersupp@gmail.com</h2>
+                    </div>
+                </ParallaxLayer>
+
+                <ParallaxLayer
                     offset={2}
                     speed={-0.3}
                     style={{
                         backgroundSize: '80%',
                         backgroundPosition: 'center',
-                        // backgroundImage: url('clients', true),
                     }}
                 />
 
@@ -85,9 +114,10 @@ export default function LandingPage() {
                     speed={0.1}
                     onClick={() => parallax.current.scrollTo(2)}
                     className='landingContent'
-
                 >
-                    <h1>What we do</h1>
+                    <div className='whatWeDo'>
+                        <h1>What we do</h1>
+                    </div>
                 </ParallaxLayer>
 
                 <ParallaxLayer
@@ -96,9 +126,11 @@ export default function LandingPage() {
                     onClick={() => parallax.current.scrollTo(0)}
                     className='landingContent'
                 >
-                    <h1>Contact Us</h1>
+                    <div className='contactUs'>
+                        <h1>Contact Us</h1>
+                    </div>
                 </ParallaxLayer>
             </Parallax>
-        </div>
+        </div >
     );
 }
