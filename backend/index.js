e91ead6e293790c7f4973491cebab467d70549d1
Aaron Hao <aaronhao100@gmail.com>
2023-08-21 10:44:39 -0700

    hide aws secret keys

--- backend/index.js
+++ backend/index.js
@@ -30,8 +30,6 @@ dotenv.config();
 
 //CHANGE THIS TO SOMETHING MORE SECURE!
 aws.config.update({
-  // secretAccessKey: 'NBhSxllkFLKY5E7GdJnYzdMbt+4ghxsfDI1fVd5B',
-  // accessKeyId: 'AKIAWU5FKKMT3QOVBFGX',
   region: 'us-east-2'
 });
 
@@ -72,26 +70,6 @@ server.listen(8800, () => console.log(`socket listening on port 8800`));
 
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
-// var storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     let uploadPath = '';
-//     console.log(req.body.category);
-//     if (req.body.category === "profile") {
-//       uploadPath = 'public/images/profile';
-//     }
-//     else if (req.body.category === "article") {
-//       uploadPath = 'public/images/article';
-//     }
-//     else {
-//       uploadPath = 'public/images';
-//     }
-//     cb(null, uploadPath);
-//   },
-//   filename: (req, file, cb) => {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-//     cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
-//   }
-// });
 
 var upload = multer({
   storage: multerS3({
@@ -105,86 +83,12 @@ var upload = multer({
 });
 
 const profilePath = "public/images/profile";
-// app.post('/api/upload-profile-pic', upload.single('image'), async (req, res, next) => {
-//   try {
-//     if (!req.file) {
-//       return res.status(400).send('No file uploaded.');
-//     }
-//     const user = await User.findById(req.body.userId);
-//     if (user.profilePicture != "") {
-//       try {
-//         await Image.findByIdAndDelete(user.profilePicture);
-//       }
-//       catch (err) {
-//         console.log("tried to delete old file");
-//         console.log(err);
-//       }
-//     }
-//     const { originalname, buffer, mimetype } = req.file;
-//     const newImage = new Image({
-//       name: originalname,
-//       data: buffer,
-//       contentType: mimetype,
-//     });
-//     user.profilePicture = newImage._id;
-//     await newImage.save();
-//     console.log(req.file.filename);
-//     user.profilePicture = req.file.filename;
-//     user.save();
-//     res.status(200).json(user);
-//   } catch (err) {
-//     console.log(err);
-//     res.send(err);
-//   }
-
-// });
-// app.post('/api/upload-profile-pic', upload.single('image'), async (req, res, next) => {
-//   try {
-//     if (!req.file) {
-//       return res.status(400).send('No file uploaded.');
-//     }
-//     const user = await User.findById(req.body.userId);
-//     const file = req.file;
-//     var Blob = req.file.buffer;
-
-//     const S3_BUCKET = 'awspathfinderbucket';
-//     const AWS_ACCESS_KEY_ID ='AKIAWU5FKKMT3QOVBFGX';
-//     const AWS_SECRET_ACCESS_KEY = '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';
-    
-//     AWS.config.update({
-//       accessKeyId: AWS_ACCESS_KEY_ID,
-//       secretAccessKey: AWS_SECRET_ACCESS_KEY
-//     });
-
-//     const s3 = new AWS.S3();
-//     var params = {
-//       Bucket: S3_BUCKET,
-//       Key: file.name,
-//       Body: Blob
-//     };
-//     s3.upload(params, function(err, data) {
-//       console.log(err, data);
-//     });
-    
-//     user.profilePicture = file.name
-//     user.save();
-//     res.status(200).json(user);
-//   } catch (err) {
-//     console.log(err);
-//     res.send(err);
-//   }
-
-// });
 app.post('/api/upload-profile-pic', upload.single('image'), async (req, res, next) => {
   try {
     if (!req.file) {
       return res.status(400).send('No file uploaded.');
     }
     const user = await User.findById(req.body.userId);
-
-    // const S3_BUCKET = 'awspathfinderbucket';
-    // const AWS_ACCESS_KEY_ID ='AKIAWU5FKKMT3QOVBFGX';
-    // const AWS_SECRET_ACCESS_KEY = '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';
     user.profilePicture = req.file.location;
     console.log(req.file);
     user.save();
